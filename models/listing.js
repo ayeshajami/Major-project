66aab672f7971420d6d07ab537a6999dfcf0e8cb
AyeshadevX <aishaasghar436@gmail.com>
2025-09-16 20:15:08 +0500

    pro

--- models/listing.js
+++ models/listing.js
@@ -2,12 +2,19 @@ const mongoose=require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema=new Schema({
-    title:String,
+    title:{
+        type:String,
+        required:true,
+    },
     description:String,
-    image:String,
+    image:{
+        type:String,
+       set:(v)=>v===""?"https://www.agoda.com/sunset-beach-club-hotel/hotel/koh-phangan-th.html?cid=1844104&ds=H5to3%2FWrfoskiBiU":v,
+    },
     price:Number,
     location:String,
     country:String,
 });
 
 const Listing=mongoose.model("Listing",listingSchema);
+module.exports=Listing;
