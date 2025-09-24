1b69b63c62c61e53226d6c0ab61477b6edb6a731
AyeshadevX <aishaasghar436@gmail.com>
2025-09-24 22:31:44 +0500

    pro

--- app.js
+++ app.js
@@ -20,16 +20,22 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended:true}));
 
 app.get("/", (req, res) => {
   res.send("hi , i am root");
 });
-
+//Index route
 app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index.ejs", { allListings });
 });
-
+//show route
+app.get("/listings/:id",async(req,res)=>{
+  let {id}=req.params;
+  const listing=await Listing.findById(id);
+  res.render("listings/show.ejs",{listing});
+});
 // app.get("/testListing", async (req, res) => {
 //   let sampleListing = new Listing({
 //     title: "My New Villa",
@@ -43,6 +49,6 @@ app.get("/listings", async (req, res) => {
 //   res.send("successful testing");
 // });
 
-app.listen(3000, () => {
+app.listen(8080, () => {
   console.log("server is started");
 });
