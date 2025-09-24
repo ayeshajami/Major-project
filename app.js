751d22ffc4257bf7b4cec8a3b05817b4c7ea20f5
AyeshadevX <aishaasghar436@gmail.com>
2025-09-24 19:39:00 +0500

    pro

--- app.js
+++ app.js
@@ -1,40 +1,49 @@
-const express=require("express");
+const express = require("express");
 const app = express();
-const mongoose=require("mongoose");
-const Listing=require("./models/listing.js");
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
 
-const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
-.then(()=>{
+  .then(() => {
     console.log("connected to DB");
-})
-.catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
-async function main(){
-    await mongoose.connect(MONGO_URL);
+async function main() {
+  await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get("/",(req,res)=>{
-    res.send("hi , i am root");
+app.get("/", (req, res) => {
+  res.send("hi , i am root");
 });
 
-app.get("/testListing",async(req,res)=>{
-    let sampleListing = new Listing({
-        title:"My New Villa",
-        description:"By the beach",
-        price:1200,
-        location:"Calangute,Goa",
-        country:"India",
-    });
-     await sampleListing.save();
-    console.log("sample was saved");
-    res.send("successful testing");
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
 });
 
-app.listen(8080,()=>{
-    console.log("server is started");
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "My New Villa",
+//     description: "By the beach",
+//     price: 1200,
+//     location: "Calangute,Goa",
+//     country: "India",
+//   });
+//   await sampleListing.save();
+//   console.log("sample was saved");
+//   res.send("successful testing");
+// });
+
+app.listen(3000, (error) => {
+  console.log(error);
+  console.log("server is started");
 });
--- models/listing.js
+++ models/listing.js
@@ -7,10 +7,18 @@ const listingSchema=new Schema({
         required:true,
     },
     description:String,
-    image:{
-        type:String,
-       set:(v)=>v===""?"https://www.agoda.com/sunset-beach-club-hotel/hotel/koh-phangan-th.html?cid=1844104&ds=H5to3%2FWrfoskiBiU":v,
-    },
+  image: {
+  filename: {
+    type: String,
+    default: "listingimage",
+  },
+  url: {
+    type: String,
+    default: "https://images.unsplash.com/photo-1598926955088-29ce45f35d5a?...",
+  },
+},
+
+
     price:Number,
     location:String,
     country:String,
